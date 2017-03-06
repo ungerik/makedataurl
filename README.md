492af6bce401b513f874095912e5fb1b25fe75de
Erik Unger <erik@erikunger.com>
2017-03-06 19:33:53 +0100

    first version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # makedataurl
-Make a data-URL from a file, optionally serve it
+Make a data-URL from a file, optionally serves it
+
+Install:
+<pre>
+go get github.com/ungerik/makedataurl
+</pre>
+
+Testride:
+<pre>
+makedataurl test-images/melbourne-cbd.jpg
+makedataurl test-images/melbourne-cbd.gif --out=url.txt
+makedataurl test-images/melbourne-cbd.png --serve=localhost:8080
+</pre>
+
+
+Usage of makedataurl:
+<pre>
+  --out string
+    	save output to file
+  --serve string
+    	serve output at this address
+</pre>
---
+++ makedataurl.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/ogier/pflag"
+	"github.com/vincent-petithory/dataurl"
+)
+
+var (
+	outFile string
+	serveAt string
+)
+
+const indexHTML = `<!DOCTYPE html><html>
+<head><meta charset="utf-8"/><title>%s</title></head>
+<body>
+<h1>%s</h1>
+<img src="%s"/>
+<hr/>
+<code style="word-wrap:break-word">%s</code>
+`
+
+func main() {
+	pflag.StringVar(&outFile, "out", "", "save output to file")
+	pflag.StringVar(&serveAt, "serve", "", "serve output at this HTTP address")
+
+	pflag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage of makedataurl:")
+		pflag.PrintDefaults()
+	}
+
+	pflag.Parse()
+
+	if len(pflag.Args()) == 0 {
+		pflag.Usage()
+		return
+	}
+
+	filename := pflag.Args()[0]
+
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	url := dataurl.EncodeBytes(data)
+
+	if outFile != "" {
+		f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY, 0660)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintln(f, url)
+		f.Close()
+	} else if serveAt == "" {
+		fmt.Println(url)
+	}
+
+	if serveAt != "" {
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintf(w, indexHTML, filename, filename, url, url)
+		})
+
+		fmt.Println("serving at:", serveAt)
+		err = http.ListenAndServe(serveAt, nil)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
